106217fb5d04596469608a6680b1e21d3b13df10
Risako Nagai <ryyyyiko03@gmail.com>
2022-02-18 13:01:08 +0900

    upadted View

---
+++ app/controllers/lists_controller.rb
@@ -0,0 +1,38 @@
+class ListsController < ApplicationController
+
+  before_action :set_list, only: [:show, :destroy]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+    @list = List.find(params[:id])
+    @bookmark = Bookmark.new
+  end
+
+  def new
+    @list = List.new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to list_path(@list)
+    else
+      # if the restaurant doesn save, what should happen?
+      # show the form again, what page was the form on?
+      render :new
+    end
+  end
+
+  private
+  def set_list
+    @list = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
+
+end
