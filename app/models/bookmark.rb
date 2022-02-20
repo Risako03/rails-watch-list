8fdcb4275a410c8e07e0c2644c6c81e9d98f7546
Risako Nagai <ryyyyiko03@gmail.com>
2022-02-20 17:18:55 +0900

    Setup Cloudinaly and active storage

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -1,4 +1,5 @@
 class Bookmark < ApplicationRecord
+  has_many_attached :photos
   belongs_to :movie
   belongs_to :list
   validates :comment, length: { minimum: 6, too_short: "%{count} characters is the minimum allowed" }
